50c6aa76b0d85157236164a59a7e31889910109a
votramysiucute <114557578+votramysiucute@users.noreply.github.com>
2022-09-28 13:26:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'heloo bnn cuti',
+    introDesc: `có 1 thứ mà cauu sẽ khong thể nghĩ nó to đến chừng nào đó chính là tình yêu tớ dành choo cauu`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'khong biet la cauu có như tớ khong',
+    desc: 'nhưng cứ nhắm mắt là tớ lại nghĩ đến cauu',
+    btnYes: 'Vẫn cứ là thích cauu=3',
+    btnNo: 'hongg=(,
+    question:'bn cuti thế kia đi vs trmyyy là hợp đôiii lắm ấy',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'yêu tớ luonn chứ chờ j nhờ^^,
+    mess: 'yêu cauu nhiều nhiều',
+    messDesc: '2 mình quá hợp luonn.',
+    btnAccept: 'chính xác',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
